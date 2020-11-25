486db42a00f6af24980affb27b16e53ff7647d8b
Nathan Robichon <nathan.robichon4@outlook.fr>
2020-11-25 15:21:25 +0100

    g

--- app.js
+++ app.js
@@ -7,7 +7,35 @@
  ************************************************************************/
 document.addEventListener("DOMContentLoaded", function (_e) {
 
-    
+
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./sw.js');
+    }
+
+    self.addEventListener('install', (e) => {
+        console.log('[Service Worker] Install');
+    });
+
+    self.addEventListener('fetch', (e) => {
+        console.log('[Service Worker] Fetched resource '+e.request.url);
+    });
+
+    self.addEventListener('fetch', (e) => {
+        e.respondWith(
+            caches.match(e.request).then((r) => {
+                console.log('[Service Worker] Fetching resource: '+e.request.url);
+                return r || fetch(e.request).then((response) => {
+                    return caches.open(cacheName).then((cache) => {
+                        console.log('[Service Worker] Caching new resource: '+e.request.url);
+                        cache.put(e.request, response.clone());
+                        return response;
+                    });
+                });
+            })
+        );
+    });
+
+
     /******************************************************************
             Fonctions à compléter dans la dernière partie du TP 
     ******************************************************************/
